06e2a69893191352b36973a1726513fb5801858e
govindbagdi <govindrajput747040@gmail.com>
2025-09-21 20:16:37 +0530

    test3

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,7 @@ export default function Footer() {
         <div className='me-5 d-none d-lg-block'>
           <span>Get connected with us on social networks:</span>
         </div>
-
+        
         <div>
           <a href='' className='me-4 text-reset'>
             <MDBIcon fab icon="facebook-f" />
--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,6 @@ import {
   MDBCardText,
   MDBCardImage,
   MDBBtn,
-  MDBRipple,
   MDBRow,
   MDBCol,
   
